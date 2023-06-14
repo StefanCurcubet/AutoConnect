5a68798c32b2a2751aa73b01a2eb8a22515e20c3
StefanCA89 <stefan.curcubet@gmail.com>
2023-06-14 13:27:02 +0300

    tweaked fetch call

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -43,7 +43,7 @@ export default function UserPage() {
             return
         }
         await rateUser(rating)
-        getSelectedUser()
+        await getSelectedUser()
         dispatch(getAllSellerRatings())
     }
 
