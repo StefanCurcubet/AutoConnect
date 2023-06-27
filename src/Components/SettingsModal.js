09e5a370a79466b955b058d41561c13376326192
StefanCA89 <stefan.curcubet@gmail.com>
2023-06-27 13:29:37 +0300

    mailing users and confirmation of email adress

--- src/Components/SettingsModal.js
+++ src/Components/SettingsModal.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Modal from "react-modal"
 import { useDispatch, useSelector } from "react-redux"
-import { setSettingsModal, updateSettings } from "../Features/userSlice";
+import { createPin, setSettingsModal, updateSettings, verifyPin } from "../Features/userSlice";
 import { FadeLoader } from "react-spinners";
 
 export default function SettingsModal() {
@@ -17,13 +17,15 @@ export default function SettingsModal() {
         },
     };
 
-    const {settingsModalOpen, settings, isLoading} = useSelector((store) => store.user)
+    const {settingsModalOpen, settings, pinCorrect, isLoading} = useSelector((store) => store.user)
     const [modifiedSettings, setModifiedSettings] = useState({
         email : '',
         notify_by_mail_message: false,
         notify_by_mail_comment: false,
     })
     const [isModified, setIsModified] = useState(false)
+    const [emailPinWindow, setEmailPinWindow] = useState(false)
+    const [emailPin, setEmailPin] = useState('')
     const [updateSent, setUpdateSent] = useState(false)
     const dispatch = useDispatch()
 
@@ -49,6 +51,25 @@ export default function SettingsModal() {
         }
     },[modifiedSettings])
 
+    function handleSave() {
+        if (modifiedSettings.email != settings.email) {
+            dispatch(createPin())
+            setEmailPinWindow(true)
+            return
+        }
+        dispatch(updateSettings(modifiedSettings),
+        setUpdateSent(true))
+    }
+
+    function handleConfirmEmail() {
+        if (!pinCorrect) {
+            dispatch(verifyPin(emailPin))
+        } else {
+            dispatch(updateSettings(modifiedSettings),
+            setUpdateSent(true))
+        }
+    }
+
     return (
         <Modal
             isOpen={settingsModalOpen}
@@ -58,7 +79,7 @@ export default function SettingsModal() {
             <div className="card">
                 <div className='card-header d-flex'>
                     <div>Settings</div>
-                    <i className="bi bi-x-lg ms-auto" style={{cursor: 'pointer'}} onClick={() => (dispatch(setSettingsModal(false)), setUpdateSent(false))}></i>
+                    <i className="bi bi-x-lg ms-auto" style={{cursor: 'pointer'}} onClick={() => (dispatch(setSettingsModal(false)), setUpdateSent(false), setEmailPinWindow(false))}></i>
                 </div>
                 <div className="card-body d-flex flex-column">
                     {updateSent ?
@@ -68,22 +89,32 @@ export default function SettingsModal() {
                             <h2 className="text-success">Settings Updated !</h2>
                     :
                         <>
-                            <label htmlFor="email" className="form-label">Email adress:</label>
-                            <input type="text" className="form-control" id="email" value={modifiedSettings?.email} onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, email : e.target.value}))} required/>
-                            <h6 className="mt-2">Notify me by email for:</h6>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="messages" onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, notify_by_mail_message : e.target.checked}))} checked={modifiedSettings?.notify_by_mail_message}/>
-                                <label className="form-check-label" htmlFor="messages">
-                                    Messages received
-                                </label>
-                            </div>
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="comments" onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, notify_by_mail_comment : e.target.checked}))} checked={modifiedSettings?.notify_by_mail_comment}/>
-                                <label className="form-check-label" htmlFor="comments">
-                                    Comments on my listings
-                                </label>
-                            </div>
-                            <button className="btn btn-success mt-2" disabled={!isModified} onClick={() => dispatch(updateSettings(modifiedSettings), setUpdateSent(true))}>Save Changes</button>
+                            {emailPinWindow ?
+                                <>
+                                    <h6>Email sent to {modifiedSettings.email} containing the validation PIN</h6>
+                                    <input type="text" className="form-control mt-1" id="pin" value={emailPin} placeholder="enter PIN here"  onChange={(e) => setEmailPin(e.target.value)} required/>
+                                    <button className="btn btn-success mt-2" onClick={() => handleConfirmEmail()}>Confirm</button>
+                                </>
+                            :
+                                <>
+                                    <label htmlFor="email" className="form-label">Email adress:</label>
+                                    <input type="text" className="form-control" id="email" value={modifiedSettings?.email} onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, email : e.target.value}))} required/>
+                                    <h6 className="mt-2">Notify me by email for:</h6>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" id="messages" onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, notify_by_mail_message : e.target.checked}))} checked={modifiedSettings?.notify_by_mail_message}/>
+                                        <label className="form-check-label" htmlFor="messages">
+                                            Messages received
+                                        </label>
+                                    </div>
+                                    <div className="form-check">
+                                        <input className="form-check-input" type="checkbox" id="comments" onChange={(e) => setModifiedSettings((prevSetting) => ({...prevSetting, notify_by_mail_comment : e.target.checked}))} checked={modifiedSettings?.notify_by_mail_comment}/>
+                                        <label className="form-check-label" htmlFor="comments">
+                                            Comments on my listings
+                                        </label>
+                                    </div>
+                                    <button className="btn btn-success mt-2" disabled={!isModified} onClick={() => handleSave()}>Save Changes</button>
+                                </>
+                            }
                         </>
                     }
                 </div>
